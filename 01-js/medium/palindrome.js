914ebcb2257345d010a6ae9e02a94ced338fc0e6
Prince Saliya <princesaliya4040@gmail.com>
2024-06-04 08:33:06 +0530

    01-js medium solved

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,7 +3,39 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+function isAlphanumeric(ch) {
+  if (
+    (ch >= "a" && ch <= "z") ||
+    (ch >= "A" && ch <= "Z") ||
+    (ch >= "0" && ch <= "9")
+  ) {
+    return true;
+  }
+  return false;
+}
+
 function isPalindrome(str) {
+  str = str.toLowerCase();
+  let i = 0;
+  let j = str.length - 1;
+
+  while (i < j) {
+    if (!isAlphanumeric(str[i])) {
+      i++;
+      continue;
+    }
+    if (!isAlphanumeric(str[j])) {
+      j--;
+      continue;
+    }
+
+    if (str[i] !== str[j]) {
+      return false;
+    }
+    i++;
+    j--;
+  }
+
   return true;
 }
 
